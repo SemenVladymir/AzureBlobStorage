7b583de8183ef313bb691bb86d380e3de6a9c83b
VladimirSemen2022 <107301979+VladimirSemen2022@users.noreply.github.com>
2024-02-15 13:36:05 +0200

    Добавьте файлы проекта.

---
+++ AzureBlobClient.cs
@@ -0,0 +1,69 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace AzureBlobStorage
+{
+    internal class AzureBlobClient
+    {
+        private readonly string connectionString = "DefaultEndpointsProtocol=https;AccountName=semen2024storage;AccountKey=WXlcCpF8jKhWhtFTMFJHTkVRCJBCDqtKmhce0IUpD+5RtLTIbaOADese66YmafU/sTc/AMew666k+AStFqoU1A==;EndpointSuffix=core.windows.net";
+        private static string containerName = "myfiles";
+        private static BlobServiceClient serviceClient;
+        private static AzureBlobClient _instance;
+        private static BlobContainerClient containerClient;
+
+        private AzureBlobClient()
+        {
+            serviceClient = new BlobServiceClient(connectionString);
+            containerClient = serviceClient.GetBlobContainerClient(containerName);
+        }
+
+        public static AzureBlobClient GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new AzureBlobClient();
+            }
+            return _instance;
+        }
+        public static async Task UploadBlob()
+        {
+            var path = "";
+            var fileName = "Testfile.txt";
+            var localFile = Path.Combine(path, fileName);
+            var blobClient = containerClient.GetBlobClient(fileName);
+            Console.WriteLine("Uploading to Blob storage");
+            using FileStream uploadFileStream = File.OpenRead(localFile);
+            await blobClient.UploadAsync(uploadFileStream, true);
+            uploadFileStream.Close();
+        }
+
+        public static async Task AddBlobFile()
+        {
+            Console.WriteLine($"Add file to blob");
+            string localPath = "C:\\Users\\User\\OneDrive\\Documents\\GitHub\\AzureBlobStorage\\bin\\Debug\\net6.0";
+            string fileName = "NewFile.txt";
+            string localFilePath = Path.Combine(localPath, fileName);
+            BlobClient blobClient = containerClient.GetBlobClient(fileName);
+            await blobClient.UploadAsync(localFilePath, true);
+        }
+
+        public static async Task DeleteBlob()
+        {
+            Console.Write("Press any key to begin clean up");
+            Console.ReadLine();
+            Console.WriteLine("Deleting blob container...");
+            await containerClient.DeleteAsync();
+            Console.WriteLine("Done");
+        }
+
+
+        public static async Task ShowFilesList()
+        {
+            await foreach (BlobItem blobItem in containerClient.GetBlobsAsync())
+            {
+                Console.WriteLine("\t" + blobItem.Name);
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using AzureBlobStorage;
+
+AzureBlobClient.GetInstance();
+string inp;
+do
+{
+    Console.Clear();
+    Console.WriteLine("What do you want to do?\n\n");
+    Console.WriteLine("1. Read all files in the Azure blob\n");
+    Console.WriteLine("2. Add new file to the Azure blob\n");
+    Console.WriteLine("3. Upload file into the Azure blob\n");
+    Console.WriteLine("4. Delete file from the Azure blob\n\n\n");
+    Console.WriteLine("0. Exit from program\n");
+    inp = Console.ReadLine();
+    if (int.TryParse(inp, out int res))
+    {
+        switch (res)
+        {
+            case 1:
+                await AzureBlobClient.ShowFilesList();
+                break;
+            case 2:
+                await AzureBlobClient.AddBlobFile();
+                break; 
+            case 3:
+                await AzureBlobClient.UploadBlob();
+                break;
+            case 4:
+                await AzureBlobClient.DeleteBlob();
+                break;
+            case 0:
+                return;
+        }
+    }
+} while (true);
